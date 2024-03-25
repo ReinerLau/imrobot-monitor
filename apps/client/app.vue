c44a938fcba1866a191899142b5a4f1fa766b925
ReinerLau <lk850593913@gmail.com>
2024-03-25 19:33:51 +0800

    feat: add element and tailwind

--- apps/client/app.vue
+++ apps/client/app.vue
@@ -2,5 +2,6 @@
   <div>
     <!-- <NuxtWelcome /> -->
     hello
+    <NuxtPage></NuxtPage>
   </div>
 </template>
---
+++ apps/client/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const onClick = () => {
+  console.log("test");
+};
+</script>
+
+<template>
+  <div class="text-red-500">
+    <el-button type="primary" @click="onClick">test</el-button>
+  </div>
+</template>
