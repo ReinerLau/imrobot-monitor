503e3d35d22e58c86cdbefd0771aa5de3e685621
ReinerLau <lk850593913@gmail.com>
2024-04-19 14:40:56 +0800

    chore: server docker config

--- examples/test-example/src/main.ts
+++ examples/test-example/src/main.ts
@@ -1,4 +1,4 @@
-import monitor, { behavior, screen } from '@imrobot/core'
+import monitor, { behavior, screen } from 'imrobot-monitor'
 
 import { createApp } from 'vue'
 import App from './App.vue'
