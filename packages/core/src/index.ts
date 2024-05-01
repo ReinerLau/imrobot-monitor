296992517c7dfdb99b5ca945addc11832c9c8461
ReinerLau <lk850593913@gmail.com>
2024-05-01 11:24:39 +0800

    refactor: behavior package rename to monitor-behavior

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,7 @@
 import { install } from "./install";
 import { Monitor } from "./types";
 import { use } from "./use";
-export { default as behavior } from "@imrobot/behavior";
+export { default as behavior } from "@imrobot/monitor-behavior";
 export { playScreen, default as screen } from "@imrobot/monitor-screen";
 
 const monitor: Monitor = {
