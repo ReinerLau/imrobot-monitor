f62e239c4f0c20cf0e2cfcc4f5ab163083fd9197
一纸忘忧 <i@ikxin.com>
2024-05-08 15:16:06 +0800

    feat(playground): record rrweb events

--- playground/vite-vue3/src/App.vue
+++ playground/vite-vue3/src/App.vue
@@ -1,4 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { record, type EventType } from "rrweb";
+import { onMounted } from "vue";
+
+const recordData: EventType[] = [];
+
+onMounted(() => {
+  record({
+    emit(event) {
+      recordData.push(event);
+    },
+  });
+});
+
+setInterval(() => {
+  console.log(recordData);
+}, 5000);
+</script>
 
 <template>
   <div></div>
