fb568d0cd04232df0373a4a94455802d66a4c75c
一纸忘忧 <i@ikxin.com>
2024-05-09 09:09:35 +0800

    feat(service): homepage project datatable

--- service/app.vue
+++ service/app.vue
@@ -1,22 +1,15 @@
-<script setup lang="ts">
-import rrwebPlayer from "rrweb-player";
-import "rrweb-player/dist/style.css";
-
-const { data } = await useFetch("/api/record");
-
-const events: any[] = data.value as [];
-
-onMounted(() => {
-  new rrwebPlayer({
-    target: document.body,
-    props: {
-      events: events[events.length - 1].data,
-      UNSAFE_replayCanvas: true,
-    },
-  });
-});
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div></div>
+  <NuxtPage />
 </template>
+
+<style>
+#__nuxt {
+  @apply h-screen;
+}
+
+html {
+  font-size: 14px;
+}
+</style>
---
+++ service/pages/index.vue
@@ -0,0 +1,40 @@
+<script lang="tsx" setup>
+import dayjs from "dayjs";
+
+const { data } = useFetch("/api/project");
+</script>
+
+<template>
+  <header class="h-16 flex items-center p-4 shadow-md">
+    <Button label="添加项目" />
+  </header>
+  <ScrollPanel class="h-[calc(100vh-96px)] m-4">
+    <DataTable :value="data">
+      <Column field="id" header="编号" sortable></Column>
+      <Column field="name" header="名称" sortable></Column>
+      <Column field="token" header="密钥" sortable></Column>
+      <Column field="createdAt" header="创建时间" sortable>
+        <template #body="{ data }">
+          <Tag :value="dayjs(data.createdAt).format('YYYY-MM-DD HH:mm:ss')" />
+        </template>
+      </Column>
+      <Column header="数据">
+        <template #body="{ data }">
+          <div class="flex gap-2">
+            <Button label="录屏" />
+            <Button label="动作" />
+            <Button label="错误" />
+          </div>
+        </template>
+      </Column>
+      <Column header="操作">
+        <template #body="{ data }">
+          <div class="flex gap-2">
+            <Button label="编辑" />
+            <Button label="删除" />
+          </div>
+        </template>
+      </Column>
+    </DataTable>
+  </ScrollPanel>
+</template>
