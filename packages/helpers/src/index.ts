a9828184110cd9cc102bc461644af52a78612673
ReinerLau <lk850593913@gmail.com>
2024-05-11 17:38:08 +0800

    refactor(helpers): refactor

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -1,10 +1,6 @@
 import { Monitor } from "./monitor";
 export * from "./monitor";
 
-export const global = {
-  baseURL: "http://localhost:3001",
-};
-
 export const getTimestamp = () => {
   return Date.now();
 };
@@ -14,25 +10,4 @@ export type extensionInstallEvent = (monitor: Monitor, options?: any) => void;
 export interface Extension {
   install: extensionInstallEvent;
   afterEvent?: Function;
-}
-
-export function reportData(url: string, data: Record<string, any>) {
-  const headers = {
-    type: "application/json",
-  };
-  const result = navigator.sendBeacon(
-    `${global.baseURL}${url}`,
-    new Blob([JSON.stringify(data)], headers)
-  );
-  if (!result) {
-    requestIdleCallback(() => {
-      fetch(`${global.baseURL}${url}`, {
-        method: "post",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-    });
-  }
 }
