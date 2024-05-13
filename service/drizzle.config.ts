58e4f462d0cfd930fb2f721c1b7c262cbb4d5e05
一纸忘忧 <i@ikxin.com>
2024-05-13 21:55:43 +0800

    chore(service): update drizzle-kit

--- service/drizzle.config.ts
+++ service/drizzle.config.ts
@@ -2,9 +2,10 @@ import "dotenv/config";
 import type { Config } from "drizzle-kit";
 
 export default {
+  dialect: "sqlite",
   out: "./server/db/migrations",
   schema: "./server/db/schema.ts",
-  driver: "better-sqlite",
+  // driver: "better-sqlite",
   dbCredentials: {
     url: "sqlite.db",
   },
