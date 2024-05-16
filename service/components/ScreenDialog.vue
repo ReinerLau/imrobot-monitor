d005ac9c726606812c109641720880cc4ab4d0b1
ReinerLau <lk850593913@gmail.com>
2024-05-16 14:10:58 +0800

    feat(service): screen dialog

---
+++ service/components/ScreenDialog.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+defineProps<{ visible: boolean }>();
+
+const emit = defineEmits(["update:visible"]);
+
+const handleHide = () => {
+  emit("update:visible", false);
+};
+</script>
+
+<template>
+  <Dialog
+    :visible="visible"
+    @update:visible="handleHide"
+    header="录屏回放"
+  ></Dialog>
+</template>
