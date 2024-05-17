57d49272749146cc118510b6bab5c05ce0424a44
一纸忘忧 <i@ikxin.com>
2024-05-17 15:44:34 +0800

    refactor(service): error code page

--- service/server/api/error/index.get.ts
+++ service/server/api/error/index.get.ts
@@ -1,16 +1,18 @@
 export default defineEventHandler(async (event) => {
-  const { token } = getHeaders(event);
+  const { token } = getQuery(event);
 
   if (token) {
     const project = await db.query.imProject.findFirst({
-      where: (imProject, { eq }) => eq(imProject.token, token),
+      where: (imProject, { eq }) => eq(imProject.token, String(token)),
     });
 
     if (project) {
-      const result = await db.query.imError.findMany({});
+      const result = await db.query.imError.findMany({
+        where: (imError, { eq }) => eq(imError.projectId, project.id),
+      });
       return result;
     }
   } else {
-    return "Not Found Token";
+    return null;
   }
 });
